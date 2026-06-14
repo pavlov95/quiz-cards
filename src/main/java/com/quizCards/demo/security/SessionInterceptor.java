8bf6627d90cd251e42a25e4cf08ca11fff5e5efb
pavlov95 <dim_pavlov@abv.bg>
2026-06-14 19:24:51 +0300

    feat: configure project and add session interceptor

---
+++ src/main/java/com/quizCards/demo/security/SessionInterceptor.java
@@ -0,0 +1,37 @@
+package com.quizCards.demo.security;
+
+import com.quizCards.demo.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Set;
+
+@Component
+public class SessionInterceptor implements HandlerInterceptor {
+
+    public static final Set<String> UNAUTHENTICATED_ENDPOINTS = Set.of("/login", "/register", "/", "/logout", "/error");
+    public static final String USER_ID_FROM_SESSION = "user_id";
+    private final UserService userService;
+
+    public SessionInterceptor(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String endpoint = request.getServletPath();
+        if (UNAUTHENTICATED_ENDPOINTS.contains(endpoint)) {
+            return true;
+        }
+
+        if (request.getSession(false) == null || request.getSession(false).getAttribute(USER_ID_FROM_SESSION) == null) {
+            response.sendRedirect("/");
+            return false;
+        }
+
+        return true;
+    }
+}
